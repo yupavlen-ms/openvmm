ce30cfd12fa76a91b4a1dbfd17ed61805d2739c8
Trevor Jones <trevor@thjmedia.net>
2024-12-17 14:57:42 -0800

    Properly serialize ACPI tables (#519)
    Adds a new vec_base64_vec module for deserializing a Vec of Vec's. Used
    for the ACPI tables.

--- vm/devices/get/get_protocol/src/dps_json.rs
+++ vm/devices/get/get_protocol/src/dps_json.rs
@@ -157,6 +157,7 @@ pub struct HclDevicePlatformSettingsV2Dynamic {
     pub is_servicing_scenario: bool,
 
     #[serde(default)]
+    #[serde(with = "serde_helpers::vec_base64_vec")]
     pub acpi_tables: Vec<Vec<u8>>,
 }
 
