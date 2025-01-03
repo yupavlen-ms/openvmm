12c78cfa642762b0181de35d1870446c26f3ab9c
John Starks <jostarks@microsoft.com>
2025-01-03 08:21:54 -0800

    lints: forbid unsafe op in unsafe fn (#604)
    We always want `unsafe {}` blocks to be explicit, even when the unsafe
    code is part of an unsafe function. Let's be consistent between `unsafe
    {}` inside and outside of `unsafe` functions and set the
    `unsafe_op_in_unsafe_fn` lint to "forbid" instead of "warn".
    
    There is no practical difference in checked in code, since we deny
    warnings in CI. But I find that for consistency I'd prefer to get errors
    instead of warnings for this pattern during local development.
    
    (Frankly `unsafe_op_in_unsafe_fn` seems like it should just be an error
    at the language level, not a lint. Embedded developers seem to have
    successfully pushed back against that.)

--- vm/devices/net/linux_net_bindings/src/lib.rs
+++ vm/devices/net/linux_net_bindings/src/lib.rs
@@ -14,24 +14,26 @@ use std::os::raw::c_int;
 
 // Generated using:
 //
-// bindgen --no-layout-tests --with-derive-default /usr/include/linux/if.h
+// bindgen --no-layout-tests --with-derive-default --wrap-unsafe-ops --no-doc-comments /usr/include/linux/if.h
 #[allow(non_camel_case_types)]
-#[expect(unsafe_op_in_unsafe_fn)]
 #[allow(dead_code)]
 #[allow(non_upper_case_globals)]
 #[allow(clippy::missing_safety_doc)]
 #[allow(clippy::undocumented_unsafe_blocks)]
+#[allow(clippy::ref_as_ptr)]
+#[allow(clippy::ptr_as_ptr)]
 pub mod gen_if;
 
 // Generated using:
 //
-// bindgen --no-layout-tests --with-derive-default /usr/include/linux/if_tun.h
+// bindgen --no-layout-tests --with-derive-default --wrap-unsafe-ops --no-doc-comments /usr/include/linux/if_tun.h
 #[allow(non_camel_case_types)]
-#[expect(unsafe_op_in_unsafe_fn)]
 #[allow(dead_code)]
 #[allow(non_upper_case_globals)]
 #[allow(clippy::missing_safety_doc)]
 #[allow(clippy::undocumented_unsafe_blocks)]
+#[allow(clippy::ref_as_ptr)]
+#[allow(clippy::ptr_as_ptr)]
 pub mod gen_if_tun;
 
 // #define TUNSETIFF     _IOW('T', 202, int)
