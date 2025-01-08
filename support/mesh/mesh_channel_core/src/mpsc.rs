41307a50384a53234578ccf2984c484e73a2df3a
John Starks <jostarks@microsoft.com>
2025-01-08 10:55:29 -0800

    mesh_channel_core: use transparent enum variant encodings (#631)
    Improve the mesh encoding of channel messages with
    `#[mesh(transparent)]`. This improves perf for in-proc encoded send/recv
    by about 15%.

--- support/mesh/mesh_channel_core/src/mpsc.rs
+++ support/mesh/mesh_channel_core/src/mpsc.rs
@@ -777,7 +777,9 @@ type EncodeFn = unsafe fn(MessagePtr) -> Message;
 #[derive(Protobuf)]
 #[mesh(bound = "T: MeshField", resource = "mesh_node::resource::Resource")]
 enum ChannelPayload<T> {
+    #[mesh(transparent)]
     Message(T),
+    #[mesh(transparent)]
     Port(Port),
 }
 
