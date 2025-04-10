a4ea9156f43cee315d2e9832b2ca23914c41bd1b
Trevor Jones <trevor@thjmedia.net>
2025-04-10 14:48:22 -0700

    ci: disable ubuntu no agent hyper-v test (#1174)
    This test is unreliable since the shutdown IC isn't always ready when it
    says it is.

--- vmm_tests/vmm_tests/tests/tests/multiarch.rs
+++ vmm_tests/vmm_tests/tests/tests/multiarch.rs
@@ -165,6 +165,8 @@ async fn reboot(config: PetriVmConfigOpenVmm) -> Result<(), anyhow::Error> {
 }
 
 /// Basic boot test without agent
+// TODO: investigate why the shutdown ic doesn't work reliably with hyper-v
+// in our ubuntu image
 #[vmm_test(
     openvmm_linux_direct_x64,
     openvmm_openhcl_linux_direct_x64,
@@ -179,9 +181,8 @@ async fn reboot(config: PetriVmConfigOpenVmm) -> Result<(), anyhow::Error> {
     openvmm_openhcl_uefi_x64(vhd(ubuntu_2204_server_x64)),
     openvmm_openhcl_uefi_x64[vbs](vhd(windows_datacenter_core_2022_x64)),
     openvmm_openhcl_uefi_x64[vbs](vhd(ubuntu_2204_server_x64)),
-    hyperv_openhcl_uefi_aarch64(vhd(ubuntu_2404_server_aarch64)),
+    // hyperv_openhcl_uefi_aarch64(vhd(ubuntu_2404_server_aarch64)),
     hyperv_openhcl_uefi_x64(vhd(windows_datacenter_core_2022_x64)),
-    // the shutdown ic doesn't work reliably with hyper-v in our ubuntu image
     // hyperv_openhcl_uefi_x64(vhd(ubuntu_2204_server_x64)),
     hyperv_openhcl_uefi_x64[vbs](vhd(windows_datacenter_core_2025_x64)),
     hyperv_openhcl_uefi_x64[tdx](vhd(windows_datacenter_core_2025_x64))
