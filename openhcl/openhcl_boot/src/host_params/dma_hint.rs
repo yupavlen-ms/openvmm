ac049360bd1caba866f94e9c162a04b1cc02638b
Yuri Pavlenko <yupavlen@microsoft.com>
2025-04-10 21:52:57 +0000

    More experiments

--- openhcl/openhcl_boot/src/host_params/dma_hint.rs
+++ openhcl/openhcl_boot/src/host_params/dma_hint.rs
@@ -3,6 +3,18 @@
 
 //! Calculate DMA hint value if not provided by host.
 
-pub fn vtl2_calculate_dma_hint() -> u64 {
+use crate::log;
+use igvm_defs::MemoryMapEntryType;
+use super::PartitionInfo;
+
+pub fn vtl2_calculate_dma_hint(vp_count: usize, storage: &PartitionInfo) -> u64 {
+    log!("YSP: vp_count = {}", vp_count);
+    let mem_size = storage
+        .vtl2_ram
+        .iter()
+        .filter(|m| m.mem_type == MemoryMapEntryType::VTL2_PROTECTABLE)
+        .map(|e| e.range.len())
+        .sum::<u64>();
+    log!("YSP: mem_size = {}", mem_size);
     16384
 }
