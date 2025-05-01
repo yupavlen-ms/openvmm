yupavlen-ms/openvmm

commit 2804aa3ee55d5d2e410d88474a64726b70b8eaaa

openhcl: update proxy_irr_filter when vmbus_relay is being used, to allow host proxied interrupts on proxied channels (#1207)
Implementation for issue
[1162](https://github.com/microsoft/openvmm/issues/1162).
`poxy_irr_filter` update logic is changed to consider whether
`vmbus_relay` is active or not, while collecting all SINT vectors, in
addition to proxy bit

openhcl/virt_mshv_vtl/src/processor/hardware_cvm/mod.rs
@@ -29,7 +29,6 @@ use hvdef::HvMapGpaFlags;
 use hvdef::HvRegisterVsmPartitionConfig;
 use hvdef::HvRegisterVsmVpSecureVtlConfig;
 use hvdef::HvResult;
-use hvdef::HvSynicSint;
 use hvdef::HvVtlEntryReason;
 use hvdef::HvX64PendingExceptionEvent;
 use hvdef::HvX64RegisterName;
@@ -1499,16 +1498,6 @@ impl<B: HardwareIsolatedBacking> UhProcessor<'_, B> {
     ) -> Result<(), MsrError> {
         let self_index = self.vp_index();
         let hv = &mut self.backing.cvm_state_mut().hv[vtl];
-        // If updated is Synic MSR, then check if its proxy or previous was proxy
-        // in either case, we need to update the `proxy_irr_blocked`
-        let mut irr_filter_update = false;
-        if matches!(msr, hvdef::HV_X64_MSR_SINT0..=hvdef::HV_X64_MSR_SINT15) {
-            let sint_curr = HvSynicSint::from(hv.synic.sint((msr - hvdef::HV_X64_MSR_SINT0) as u8));
-            let sint_new = HvSynicSint::from(value);
-            if sint_curr.proxy() || sint_new.proxy() {
-                irr_filter_update = true;
-            }
-        }
 
         let mut access = HypercallOverlayAccess {
             vtl,
@@ -1522,8 +1511,8 @@ impl<B: HardwareIsolatedBacking> UhProcessor<'_, B> {
         let r = hv.msr_write(msr, value, &mut access);
 
         if !matches!(r, Err(MsrError::Unknown)) {
-            // Check if proxy filter update was required (in case of SINT writes)
-            if irr_filter_update {
+            // If updated is Synic MSR, then update the `proxy_irr_blocked`
+            if matches!(msr, hvdef::HV_X64_MSR_SINT0..=hvdef::HV_X64_MSR_SINT15) {
                 self.update_proxy_irr_filter(vtl);
             }
         }

openhcl/virt_mshv_vtl/src/processor/mod.rs
@@ -1188,7 +1188,9 @@ impl<'a, T: Backing> UhProcessor<'a, T> {
             for sint in 0..NUM_SINTS as u8 {
                 let sint_msr = hv.synic.sint(sint);
                 let hv_sint = HvSynicSint::from(sint_msr);
-                if hv_sint.proxy() && !hv_sint.masked() {
+                // When vmbus relay is active, then SINT will not be proxied
+                // in case of non-relay, guest will setup proxied sint
+                if (hv_sint.proxy() || self.partition.vmbus_relay) && !hv_sint.masked() {
                     irr_bits.set(hv_sint.vector() as usize, true);
                 }
             }

openhcl/underhill_core/src/worker.rs
@@ -1748,6 +1748,7 @@ async fn new_underhill_vm(
         crash_notification_send,
         vmtime: &vmtime_source,
         cvm_params,
+        vmbus_relay: with_vmbus_relay,
     };
 
     let (partition, vps) = proto_partition

openhcl/virt_mshv_vtl/src/lib.rs
@@ -225,6 +225,7 @@ struct UhPartitionInner {
     // N.B For now, only one device vector table i.e. for VTL0 only
     #[inspect(with = "|x| inspect::iter_by_index(x.read().into_inner().map(inspect::AsHex))")]
     device_vector_table: RwLock<IrrBitmap>,
+    vmbus_relay: bool,
 }
 
 #[derive(Inspect)]
@@ -1333,6 +1334,8 @@ pub struct UhLateParams<'a> {
     pub vmtime: &'a VmTimeSource,
     /// Parameters for CVMs only.
     pub cvm_params: Option<CvmLateParams>,
+    /// vmbus_relay is enabled and active for partition
+    pub vmbus_relay: bool,
 }
 
 /// CVM-only parameters to [`UhProtoPartition::build`].
@@ -1749,6 +1752,7 @@ impl<'a> UhProtoPartition<'a> {
             #[cfg(guest_arch = "x86_64")]
             device_vector_table: RwLock::new(IrrBitmap::new(Default::default())),
             intercept_debug_exceptions: params.intercept_debug_exceptions,
+            vmbus_relay: late_params.vmbus_relay,
         });
 
         if cfg!(guest_arch = "x86_64") {

tmk/tmk_vmm/src/paravisor_vmm.rs
@@ -62,6 +62,7 @@ impl RunContext<'_> {
                 crash_notification_send: mesh::channel().0,
                 vmtime: self.vmtime_source,
                 cvm_params: None,
+                vmbus_relay: false,
             })
             .await?;
 

commit 1f63b4ae8318ff9c24f0612b2e6ed3c7beed4ae8

virt_mshv_vtl: Don't try to update proxy_irr when VTL 1 touches its synic (#1275)
Fixes a VSM regression introduced by #1207.

openhcl/virt_mshv_vtl/src/processor/hardware_cvm/mod.rs
@@ -1510,11 +1510,12 @@ impl<B: HardwareIsolatedBacking> UhProcessor<'_, B> {
         };
         let r = hv.msr_write(msr, value, &mut access);
 
-        if !matches!(r, Err(MsrError::Unknown)) {
-            // If updated is Synic MSR, then update the `proxy_irr_blocked`
-            if matches!(msr, hvdef::HV_X64_MSR_SINT0..=hvdef::HV_X64_MSR_SINT15) {
-                self.update_proxy_irr_filter(vtl);
-            }
+        // If the MSR is a synic MSR, then update the `proxy_irr_blocked`
+        if vtl == GuestVtl::Vtl0
+            && !matches!(r, Err(MsrError::Unknown))
+            && matches!(msr, hvdef::HV_X64_MSR_SINT0..=hvdef::HV_X64_MSR_SINT15)
+        {
+            self.update_proxy_irr_filter(vtl);
         }
         r
     }