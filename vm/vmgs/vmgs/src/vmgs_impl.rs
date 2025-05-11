a54e86919bef9eb4b7339035ba29b15c625f67f2
John Starks <jostarks@microsoft.com>
2025-05-11 10:17:33 -0700

    vmgs: fix overflow in test_logger() (#1336)
    The test fails due to overflow 1/256 of the time. Fix this by changing
    the nonce manipulation to avoid overflow.

--- vm/vmgs/vmgs/src/vmgs_impl.rs
+++ vm/vmgs/vmgs/src/vmgs_impl.rs
@@ -2603,7 +2603,7 @@ mod tests {
         let fcb = vmgs.fcbs.get_mut(&FileId::BIOS_NVRAM).unwrap();
 
         // Manipulate the nonce and expect the read to fail.
-        fcb.nonce[0] += 1;
+        fcb.nonce[0] ^= 1;
 
         // read and expect to fail
         let result = vmgs.read_file(FileId::BIOS_NVRAM).await;
