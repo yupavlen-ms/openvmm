538767bdd65905a68bd02edcb5cd45d1a8143203
Yuri Pavlenko <yupavlen@microsoft.com>
2025-05-16 10:38:46 -0700

    Print error message on restore failure

--- openhcl/underhill_core/src/nvme_manager.rs
+++ openhcl/underhill_core/src/nvme_manager.rs
@@ -109,9 +109,17 @@ impl NvmeManager {
         let task = driver.spawn("nvme-manager", async move {
             // Restore saved data (if present) before async worker thread runs.
             if saved_state.is_some() {
-                let _ = NvmeManager::restore(&mut worker, saved_state.as_ref().unwrap())
+                match NvmeManager::restore(&mut worker, saved_state.as_ref().unwrap())
                     .instrument(tracing::info_span!("nvme_manager_restore"))
-                    .await;
+                    .await
+                {
+                    Ok(_) => {
+                        // Do nothing, the span above is good indicator.
+                    }
+                    Err(e) => {
+                        tracing::error!("failed to restore nvme manager: {}", e);
+                    }
+                }
             };
             worker.run(recv).await
         });
