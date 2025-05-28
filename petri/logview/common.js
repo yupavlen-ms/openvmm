e565e1ba259680e3ef35149e1a00948ad6ccb281
John Starks <jostarks@microsoft.com>
2025-05-28 13:50:47 -0700

    petri/logview: prevent some client-side xss attacks (#1429)
    Avoid using `innerHTML` to avoid HTML fragments leaking into the page.

--- petri/logview/common.js
+++ petri/logview/common.js
@@ -11,3 +11,19 @@ function convertTestName(name) {
 
 /// Base blob storage URL for the test results.
 const baseUrl = "https://openvmmghtestresults.blob.core.windows.net/results";
+
+/// Creates a node with the specified tag, attributes, and content.
+function node(tag, attrs = {}, ...content) {
+    const element = document.createElement(tag);
+    for (const [key, value] of Object.entries(attrs)) {
+        if (key === 'class') {
+            element.className = value;
+        } else if (typeof value === 'object' && value.constructor === Object) {
+            Object.assign(element[key], value);
+        } else {
+            element.setAttribute(key, value);
+        }
+    }
+    element.append(...content);
+    return element;
+}
