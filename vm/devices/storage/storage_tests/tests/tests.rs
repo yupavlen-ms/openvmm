694a1713f641cad1c3514243e3a523d8425652b4
Eric Newberry <ericnewberry@microsoft.com>
2025-06-10 18:56:23 -0700

    storage_tests: Add tests for NVMe-backed SCSI DVD (#928)
    These tests use a ramdisk-backed NVMe namespace as the backing storage
    for a SCSI DVD device.

---
+++ vm/devices/storage/storage_tests/tests/tests.rs
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+//! Tests for storage devices that don't qualify as unit tests, including integration tests.
+mod scsidvd_nvme;
