fa2470903934043907eb4d3ad3717f75022b6b1b
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2025-06-18 10:41:47 -0700

    deps: update mu_msvm to release v25.1.4 (#1533)
    Update mu_msvm to point to latest release: mu_msvm release
    (https://github.com/microsoft/mu_msvm/releases/tag/v25.1.4)

--- flowey/flowey_lib_hvlite/src/_jobs/cfg_versions.rs
+++ flowey/flowey_lib_hvlite/src/_jobs/cfg_versions.rs
@@ -23,7 +23,7 @@ pub const MDBOOK: &str = "0.4.40";
 pub const MDBOOK_ADMONISH: &str = "1.18.0";
 pub const MDBOOK_MERMAID: &str = "0.14.0";
 pub const RUSTUP_TOOLCHAIN: &str = "1.87.0";
-pub const MU_MSVM: &str = "25.1.3";
+pub const MU_MSVM: &str = "25.1.4";
 pub const NEXTEST: &str = "0.9.96";
 pub const NODEJS: &str = "18.x";
 // N.B. Kernel version numbers for dev and stable branches are not directly
