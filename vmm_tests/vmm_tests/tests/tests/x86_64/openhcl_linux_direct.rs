0357affb34636db72661cd6f167b206d2ef8e913
Matt LaFayette (Kurjanowicz) <mattkur@microsoft.com>
2025-07-18 15:14:02 -0700

    vmm_tests: craft a servicing test with 8 nvme devices (#1724)
    Needs #1722 to pass, so leaving as draft for now.
    
    The motivation for this test case is to chase down some performance
    issues across OpenHCL servicing.

--- vmm_tests/vmm_tests/tests/tests/x86_64/openhcl_linux_direct.rs
+++ vmm_tests/vmm_tests/tests/tests/x86_64/openhcl_linux_direct.rs
@@ -155,8 +155,119 @@ fn new_test_vtl2_nvme_device(
     }
 }
 
-/// Test an OpenHCL Linux direct VM with a SCSI disk assigned to VTL2, and
-/// vmbus relay. This should expose a disk to VTL0 via vmbus.
+/// Test an OpenHCL Linux direct VM with many NVMe devices assigned to VTL2 and vmbus relay.
+#[openvmm_test(openhcl_linux_direct_x64 [LATEST_LINUX_DIRECT_TEST_X64])]
+async fn many_nvme_devices_servicing(
+    config: PetriVmBuilder<OpenVmmPetriBackend>,
+    (igvm_file,): (ResolvedArtifact<impl petri_artifacts_common::tags::IsOpenhclIgvm>,),
+) -> Result<(), anyhow::Error> {
+    const NUM_NVME_DEVICES: usize = 8;
+    const SIZE: u64 = 0x1000;
+    // Zeros make it easy to see what's going on when inspecting logs. Each device must be
+    // associated with a unique GUID. The pci subsystem uses the data2 field to differentiate
+    // devices.
+    const BASE_GUID: Guid = guid::guid!("00000000-0000-0000-0000-000000000000");
+    // (also to make it obvious when looking at logs)
+    const GUID_UPDATE_PREFIX: u16 = 0x1110;
+    const NSID_OFFSET: u32 = 0x10;
+
+    let (mut vm, agent) = config
+        .with_vmbus_redirect(true)
+        .modify_backend(|b| {
+            b.with_custom_config(|c| {
+                let device_ids = (0..NUM_NVME_DEVICES)
+                    .map(|i| {
+                        let mut g = BASE_GUID;
+                        g.data2 = g.data2.wrapping_add(i as u16) + GUID_UPDATE_PREFIX;
+                        (NSID_OFFSET + i as u32, g)
+                    })
+                    .collect::<Vec<_>>();
+
+                c.vpci_devices.extend(
+                    device_ids
+                        .iter()
+                        .map(|(nsid, guid)| new_test_vtl2_nvme_device(*nsid, SIZE, *guid, None)),
+                );
+            })
+            .with_custom_vtl2_settings(|v| {
+                let device_ids = (0..NUM_NVME_DEVICES)
+                    .map(|i| {
+                        let mut g = BASE_GUID;
+                        g.data2 = g.data2.wrapping_add(i as u16) + GUID_UPDATE_PREFIX;
+                        (NSID_OFFSET + i as u32, g)
+                    })
+                    .collect::<Vec<_>>();
+
+                v.dynamic.as_mut().unwrap().storage_controllers.push(
+                    vtl2_settings_proto::StorageController {
+                        instance_id: Guid::new_random().to_string(),
+                        protocol: vtl2_settings_proto::storage_controller::StorageProtocol::Scsi
+                            .into(),
+                        luns: device_ids
+                            .iter()
+                            .map(|(nsid, guid)| vtl2_settings_proto::Lun {
+                                // Add 1 so as to avoid any confusion with booting from LUN 0 (on the implicit SCSI
+                                // controller created by the above `config.with_vmbus_redirect` call above).
+                                location: (*nsid - NSID_OFFSET) + 1,
+                                device_id: Guid::new_random().to_string(),
+                                vendor_id: "OpenVMM".to_string(),
+                                product_id: "Disk".to_string(),
+                                product_revision_level: "1.0".to_string(),
+                                serial_number: "0".to_string(),
+                                model_number: "1".to_string(),
+                                physical_devices: Some(vtl2_settings_proto::PhysicalDevices {
+                                    r#type:
+                                        vtl2_settings_proto::physical_devices::BackingType::Single
+                                            .into(),
+                                    device: Some(vtl2_settings_proto::PhysicalDevice {
+                                        device_type:
+                                            vtl2_settings_proto::physical_device::DeviceType::Nvme
+                                                .into(),
+                                        device_path: guid.to_string(),
+                                        sub_device_path: *nsid,
+                                    }),
+                                    devices: Vec::new(),
+                                }),
+                                ..Default::default()
+                            })
+                            .collect(),
+                        io_queue_depth: None,
+                    },
+                )
+            })
+        })
+        .run()
+        .await?;
+
+    for _ in 0..3 {
+        agent.ping().await?;
+
+        // Test that inspect serialization works with the old version.
+        vm.test_inspect_openhcl().await?;
+
+        vm.restart_openhcl(
+            igvm_file.clone(),
+            OpenHclServicingFlags {
+                enable_nvme_keepalive: false,
+                ..Default::default()
+            },
+        )
+        .await?;
+
+        agent.ping().await?;
+
+        // Test that inspect serialization works with the new version.
+        vm.test_inspect_openhcl().await?;
+    }
+
+    agent.power_off().await?;
+    assert_eq!(vm.wait_for_teardown().await?, HaltReason::PowerOff);
+
+    Ok(())
+}
+
+/// Test an OpenHCL Linux direct VM with a SCSI disk assigned to VTL2, an NVMe disk assigned to VTL2, and
+/// vmbus relay. This should expose two disks to VTL0 via vmbus.
 #[openvmm_test(openhcl_linux_direct_x64)]
 async fn storvsp(config: PetriVmBuilder<OpenVmmPetriBackend>) -> Result<(), anyhow::Error> {
     const NVME_INSTANCE: Guid = guid::guid!("dce4ebad-182f-46c0-8d30-8446c1c62ab3");
